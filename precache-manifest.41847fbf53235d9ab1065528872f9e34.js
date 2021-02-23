24ef1269e1d20c375796359dc0fcfff25b726151
zawarapurvaBITS <2020MT93249@wilp.bits-pilani.ac.in>
2021-02-24 00:16:08 +0530

    Updates

---
+++ precache-manifest.41847fbf53235d9ab1065528872f9e34.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbcd48df68c46735c8afaf1031f36a5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c40efd648edb7fa48d28",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "f45cce66f2cca441b816",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "14d038228b37484da807",
+    "url": "/static/css/main.fda9eeb4.chunk.css"
+  },
+  {
+    "revision": "156edabce610018f542c",
+    "url": "/static/js/2.35731d87.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.35731d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40efd648edb7fa48d28",
+    "url": "/static/js/3.5ab9c2d6.chunk.js"
+  },
+  {
+    "revision": "f45cce66f2cca441b816",
+    "url": "/static/js/4.0fb46604.chunk.js"
+  },
+  {
+    "revision": "14d038228b37484da807",
+    "url": "/static/js/main.a8b5e0de.chunk.js"
+  },
+  {
+    "revision": "bcc7abf0efc85c8e86a3",
+    "url": "/static/js/runtime-main.85f4f607.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "418c9f3e0ea71690e9ace4ff1ccc87bb",
+    "url": "/static/media/mercedes.418c9f3e.png"
+  },
+  {
+    "revision": "a5ca1e70319a364c12c4b9c5dd7a53b0",
+    "url": "/static/media/nad-1.a5ca1e70.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
